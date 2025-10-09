ff9ee399c40492852b2f1ec0a50bf9617e4aebbd
Vidyesh Churi <vidyesh95@gmail.com>
2025-10-09 10:59:02 +0530

    python:fastapi stage

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="Qatoto Backend API", description="API for Qatoto Frontend", version="0.1.0")
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Qatoto Backend API"}
 
 
 @app.get("/hello/{name}")
